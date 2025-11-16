99945f4a86f307628087b5eb137f29361ef8c6ca
Averroes098 <muhammadnafi.2023@student.uny.ac.id>
2025-11-16 20:23:02 +0700

    Update code

--- app.py
+++ app.py
@@ -143,4 +143,5 @@ def steam_details(appid):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
